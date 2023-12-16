5dbaa961762e71108684a9d11a9cc3176bd463a5
Elegant Map <elegant.map8525@fastmail.com>
2023-12-17 01:09:03 +0800

    Add compile

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,15 @@ mod ast;
 mod codegen;
 mod parse;
 mod typecheck;
+
+use std::io;
+
+use codegen::gen;
+use parse::parse;
+use typecheck::check;
+
+pub fn compile<Write: io::Write>(src: &str, dest: &mut Write) {
+    let ast = parse(src);
+    check(ast.clone()).unwrap();
+    gen(ast, dest).unwrap();
+}
